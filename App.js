3eb2a2c194c70d826d69bf51305193b233c93d59
Nik <nik@nikantonelli.com>
2020-02-21 16:58:09 +0000

    Almost there with charts

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ Ext.define('CustomApp', {
     config: {
         defaultSettings: {
             periodName: 'Day',
-            periodCount: 32,
+            periodCount: 64,
             endDate: new Date()
         }
 
@@ -18,27 +18,26 @@ Ext.define('CustomApp', {
         }
     ],
 
-    _getParents: function() {
-        var me = this;
+    _getParents: function(date) {
         var parents = Ext.create('Deft.Deferred');
         var promises = [];
         _.each(this.types, function(type) {
             promises.push( function(){
                 var deferred = Ext.create('Deft.Deferred');
                 var filters = null;
-                if (type.typePath === 'hierarchicalrequirement') {
+                if (type.typePath === 'HierarchicalRequirement') {
                     //With userstories, you can ask for the field which is the name of the first level portfolio item and 
-                    //tis field is cascaded down the userstory child hierarchy. So rather than asking for Parent or PortfolioItem,
+                    //this field is cascaded down the userstory child hierarchy. So rather than asking for Parent or PortfolioItem,
                     //we ask for 'Feature' (or whatever it is called), we can ignore the parent-child userstory tree.
-                    filters = Rally.data.wsapi.Filter.fromQueryString('('+type.parentType.name + '.ObjectID >0)');
+                    filters = { property: type.parentType.name, operator: '>', value: 0 };
                 }
                 else {
-                    filters = Rally.data.wsapi.Filter.fromQueryString('(Parent.ObjectID >0)')
+                    filters = { property: 'Parent', operator: '>', value: 0 };
                 }
 
-                this._loadRecordCount(type.typePath, filters, type.name).then({
+                this._loadRecordCount(type.typePath, filters, type.name, date).then({
                     success: function(result) {
-                        me.parented.push(result);
+//                        me.parented.push(result);
                         deferred.resolve(result);
                     },
                     failure: function(result) {
@@ -51,27 +50,30 @@ Ext.define('CustomApp', {
         }, this);
         Deft.Chain.parallel(promises, this).then({
             success: function(results) {
-                parents.resolve(results);
+                var pointParents = {};
+                _.each(results, function(result) {
+                    pointParents = Ext.merge(pointParents,result);
+                });
+                parents.resolve(pointParents);
             },
             failure: function(results) {
                 console.log('GetTotals Failed: ', results);
                 parents.reject(results);
             }
-        })
+        });
         return parents;
 
     },
 
-    _getTotals: function() {
-        var me = this;
+    _getTotals: function(date) {
         var totals = Ext.create('Deft.Deferred');
         var promises = [];
         _.each(this.types, function(type) {
             promises.push( function(){
                 var deferred = Ext.create('Deft.Deferred');
-                this._loadRecordCount(type.typePath, Rally.data.wsapi.Filter.fromQueryString('(ObjectID >0)'), type.name).then({
+                this._loadRecordCount(type.typePath, { property: 'ObjectID', operator: '>', value: 0}, type.name, date).then({
                     success: function(result) {
-                        me.totals.push(result);
+//                        me.totals.push(result);
                         deferred.resolve(result);
                     },
                     failure: function(result) {
@@ -84,44 +86,114 @@ Ext.define('CustomApp', {
         }, this);
         Deft.Chain.parallel(promises, this).then({
             success: function(results) {
-                totals.resolve(results);
+                var pointTotals = {};
+                _.each(results, function(result) {
+                    pointTotals = Ext.merge(pointTotals,result);
+                });
+                totals.resolve(pointTotals);
             },
             failure: function(results) {
                 console.log('GetTotals Failed: ', results);
                 totals.reject(results);
             }
-        })
+        });
         return totals;
     },
 
-    _updateChart: function() {
-        debugger;
+    _processResults: function(results) {
+        var me = this;
+        //Get the keys from the first bit of data. We assume they are going to be consisten throughout the whole set.
+        var keys = Object.keys(results[0].point[0]);
+        _.each(results, function(result) {
+            _.each(keys, function(key) {
+                var p = _.find(me.series, function(series) {
+                    return series.name === key;
+                });
+                p.data.push((result.point[1][key] / ((result.point[0][key]>0)?result.point[0][key]:1)) *100);
+            });
+        });
+    },
+
+    _updateChart: function(results) {
+        this._processResults(results);
+        if (this.down('#trendChart')) { this.down('trendChart').destroy();}
+
+        this.add( {
+            xtype: 'rallychart',
+            itemId: 'trendChart',
+            height: this.getHeight() - this.down('#option_box').getHeight(),
+            chartData: {
+                categories: this.categories,
+                series: this.series
+            },
+            chartConfig: {
+                chart: {
+                    type: 'line'
+                },
+                title: {
+                    text: 'Percent Correct Parentage'
+                },
+                xAxis: {
+                    type: 'datetime',
+                    dateTimeLabelFormats: 
+                    {
+                        day: "%e-%b-%y",
+                        month: "%b-%y"
+                    }                
+                },
+                yAxis: {
+                    min: 0,
+                    max: 100,
+                    title: {
+                        text: 'Percentage'
+                    }
+                },
+                tooltip: {
+                    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                        '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+                        footerFormat: '</table>',
+                        shared: true,
+                        useHTML: true
+                },
+                plotOptions: {
+                    column: {
+                        pointPadding: 0.2,
+                            borderWidth: 0
+                    }
+                }
+            }
+        });
     },
     
     types: [],
-    totals: [],
-    parented: [],
-    unparented: [],
 
-    _getData: function(types) {
+    categories: [],
+    series: [],
+
+    _getData: function(loopDate) {
+        var deferred = Ext.create('Deft.Deferred');
         var me = this;
-        me.types = types;
-        Deft.Chain.sequence([
-            this._getTotals,
-            this._getParents
-        ], me). then({
-             success: function(results) {
-                console.log('Totals Succeeded: ',results);
-             },
-             failure: function(results) {
-                console.log('Totals Failed: ',results);
-             },
-             scope: me
-         }).always( function() {
-             me._updateChart();
-         });
-        
 
+        var sequence = [];
+        sequence.push(this._getTotals(loopDate));
+        sequence.push(this._getParents(loopDate));
+        console.log(sequence);
+        Deft.Promise.all(sequence). then({
+            success: function(results) {
+                deferred.resolve({
+                    date: loopDate,
+                    point: results
+                });
+            },
+            failure: function(result) {
+                console.log('Totals Failed: ',result);
+                deferred.reject(result);
+            },
+            scope: me
+        });
+        
+        return deferred.promise;
     },
 
     launch: function() {
@@ -131,10 +203,11 @@ Ext.define('CustomApp', {
                 //It might make sense to add Defects here, but the numbers might seem
                 //a little skewed because some defects are not meant to have a parent.
                 types.splice(0,0, {
-                    typePath: 'hierarchicalrequirement',
+                    typePath: 'HierarchicalRequirement',
                     name: 'User Story',
                     parentType: types[0]
                 });
+                me.types = types;
                 var topBox = me.down('#option_box');
                 _.each(types, function(type) {
                     topBox.add( {
@@ -151,26 +224,61 @@ Ext.define('CustomApp', {
                     });
                 });
                 
-                me._getData(types);
+                _.each(types, function(type) {
+                    me.series.push( {
+                        name : type.name,
+                        data: []
+                    });
+                });
+        
+                var pCount = this.getSetting('periodCount') || this.config.defaultSettings.periodCount;
+                var pSize = 0;  //Going to default to monthly if zero
+                switch( this.getSetting('periodName')) {
+                    case 'Day':
+                        pSize = 1; break;
+                    case 'Week':
+                        pSize = 7; break;
+                    case 'Fortnight':
+                        pSize = 14; break;
+                    default:
+                        break;
+                }
+                var pDuration = pSize ? (Ext.Date.DAY) : (Ext.Date.MONTH);
+                var date = new Date(this.getSetting('endDate') || Ext.Date.now());
+                var dateFormat = pSize ? 'j/n/y' : 'M, y';
+                var loopFunctions = [];
+                var dates = [];
+                for (var i = 0; i < pCount; i++) {
+                    dates.push(Ext.Date.subtract(date, pDuration, pSize?(pSize*i):1));
+                }
 
-                //debugger;
+                dates.every( function(item) {
+                    me.categories.push(Ext.Date.format(item,dateFormat));
+                    loopFunctions.push(function() { 
+                        me.setLoading('Fetching data for: ' + Ext.Date.format(item, dateFormat));
+                        return me._getData(item); });
+                    return true;
+                });
+                
+                Deft.Chain.sequence(loopFunctions).then ({
+                    success: function(results){
+                        me._updateChart(results);
+                    },
+                    failure: function(result) {
+                        console.log('Failed to execute series loop: ', result);
+                        debugger;
+                    }
+                }).always(function() {
+                    me.setLoading(false);
+                });
 
-                /** So the stats we are after are:
-                 *  For stories:
-                 *          How many do not have Features (or Stories) as parents
-                 *          How many do have Features as parents
-                 *          How many do have Stories as parents
-                 *  For Features
-                 *          How many do not have Initiatives as parents
-                 *          How many do have Initiatives as parents
-                 */
             },
             failure: function() {
                 console.log('Failed to fetch portfolioitem types');
                 debugger;
             },
             scope: me
-        })
+        });
     },
 
     getSettingsFields: function() {
@@ -204,24 +312,39 @@ Ext.define('CustomApp', {
                 minValue: 3,
                 maxValue: 32,
                 fieldLabel: 'Period Count',
-                value: 14
+                value: 1
             },
             {
                 name: 'endDate',
                 fieldLabel: 'End Date',
                 xtype: 'rallydatefield'
             }
-        ]
+        ];
     },
 
-    _loadRecordCount: function(model, filters, id){
+    _loadRecordCount: function(model, filters, id, date){
         var deferred = Ext.create('Deft.Deferred');
         var me = this;
-        console.log("Starting load: model>>",model, 'filters>>', filters.toString());
+        console.log("Starting load: ",date, " >> ",model, 'filters >> ', JSON.stringify(filters));
 
-        Ext.create('Rally.data.wsapi.Store', {
-            model: model,
-            filters: filters,
+        var localFilters = [
+            {
+                property: '_TypeHierarchy',
+                operator: 'in',
+                value: [model]
+            },
+            {
+                property: '__At',
+                value: Ext.Date.format(date, "Y-m-d\\TH:i:s")
+            },
+            {
+                property: '_ProjectHierarchy',
+                value: me.getContext().getProject().ObjectID
+            }
+        ];
+        localFilters = localFilters.concat(filters);
+        Ext.create('Rally.data.lookback.SnapshotStore', {
+            filters: localFilters,
             limit: 1,
             pageSize: 1
         }).load({
@@ -273,7 +396,7 @@ Ext.define('CustomApp', {
                     _.each(records, function(d){
                         //Use ordinal to make sure the lowest level portfolio item type is the first in the array.
                         var idx = Number(d.get('Ordinal'));
-                        portfolioItemTypes[idx] = { typePath: d.get('TypePath').toLowerCase(), name: d.get('Name') };
+                        portfolioItemTypes[idx] = { typePath: d.get('TypePath'), name: d.get('Name') };
                         //portfolioItemTypes.reverse();
                     });
                     //Now add the parent type in
